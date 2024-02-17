d862318506661ea7b69d79480b04d397fea41604
vishal <93590077+JaY072002@users.noreply.github.com>
2024-02-17 14:41:56 +0530

    .env.example, README.md added

---
+++ README.md
@@ -0,0 +1,9 @@
+# REACT SERVER START
+cd /client
+npm i
+npm start
+
+# NODE SERVER START
+cd /server
+npm i
+npm start
--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const archiver = require('archiver');
 require('dotenv').config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
 
 app.use(cors());
 
